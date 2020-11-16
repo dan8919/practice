8107eb0908cfc53864c1a35d2dbf8e8693f53ba2
dan8919 <cuixindan1@naver.com>
2020-11-16 17:30:41 +0900

    디렉토리 내에 디렉토리가 존재하면 그 디렉토리 정보 추출하여 파일로 정리.(recursive function 사용)

--- src/main/java/data/file2/FileInfo.java
+++ src/main/java/data/file2/FileInfo.java
@@ -1,31 +1,62 @@
 package data.file2;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileInfo {
+
     public static void main(String[] args) throws IOException {
         String dir = "C:" + File.separator + "Users" + File.separator + "pharos" + File.separator + "Downloads";
+        String detailsURI = "C:" + File.separator + "Users" + File.separator + "pharos" + File.separator +"details.txt";
+
         File directory = new File(dir);
+        File details = new File(detailsURI);
 
-       File[] listFiles = directory.listFiles();
-       String dirName = directory.getName();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(details));
 
-//        File recording = new File();
+        recursive(directory,writer);
 
-       for (File f:listFiles){
-          String name= f.getName();
-           Path path = f.toPath();
-           Long size = Files.size(path);
-
-           System.out.println(name);
-           System.out.println(path);
-           System.out.println(size);
-       }
+         writer.close();
+    }
 
 
+
+    private static void recursive(File f,BufferedWriter writer)  {
+        long lsize = 0;
+        String name;
+        Path path;
+
+        if(f.isDirectory()){
+            File[] listFiles1= f.listFiles();
+            for (File f1:listFiles1){
+                name= f1.getName();
+                path = f1.toPath();
+                try {
+                      lsize = Files.size(path);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+                String size = Long.toString(lsize);
+                System.out.println(name);
+                System.out.println(path);
+                System.out.println(size);
+
+                try{
+                    writer.write(name + ":  ");
+                    writer.write(size + "\n");
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+
+                recursive(f1,writer);
+            }
+        }
 
     }
 
