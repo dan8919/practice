0d3782073def4665149981f7e1140ff3616eeda2
dan8919 <cuixindan1@naver.com>
2020-12-30 11:13:42 +0900

    list 17

--- src/main/java/array/Ex_17.java
+++ src/main/java/array/Ex_17.java
@@ -1,11 +1,23 @@
 package array;
 
+import java.util.Arrays;
+
 public class Ex_17 {
-    public static void main(String[] args) {
-        for(int i = 0, l = 5; i < l;){
-            System.out.println(i);
-            i++;
-        }
-    }
+    //check if the sum of all the 10's in the array is exactly 30. Return false if the condition does not satisfy, otherwise true.
+//    public static void main(String[] args) {
+//        int[] array_nums = {10, 77, 10, 54, -11, 10};
+//        System.out.println("Original Array: "+ Arrays.toString(array_nums));
+//        int search_num = 10;
+//        int fixed_sum = 30;
+//
+//        System.out.println("Result:"+result(array_nums,search_num,fixed_sum));
+//    }
+
+//    private static String result(int[] numbers, int search_num, int fixed_sum) {
+//        int temp_sum = 0;
+//        for (int numner:numbers){
+//            if (number == ser)
+//        }
+//    }
 
 }
